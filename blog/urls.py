98c7aa3cd1fa45d0e8ca58bf64a72cd1ed110b8d
Stévillis Sousa <stevillis@hotmail.com>
2021-11-05 21:32:48 -0400

    List all posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from blog.views import *
 
 urlpatterns = [
+    path('', index, name='index'),
     path('inserir_categoria/', inserir_categoria, name='inserir_categoria'),
     path('visualizar_categorias/', visualizar_categorias, name='visualizar_categorias'),
     path('editar_categoria/<int:id>', editar_categoria, name='editar_categoria'),
