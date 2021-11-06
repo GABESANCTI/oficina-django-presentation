2b4088343074cc1fa499cdad517257c17255bbec
Stévillis Sousa <stevillis@hotmail.com>
2021-11-05 21:06:07 -0400

    Delete Categoria

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('inserir_categoria/', inserir_categoria, name='inserir_categoria'),
     path('visualizar_categorias/', visualizar_categorias, name='visualizar_categorias'),
     path('editar_categoria/<int:id>', editar_categoria, name='editar_categoria'),
+    path('excluir_categoria/<int:id>', excluir_categoria, name='excluir_categoria'),
 ]
