86bd3f395528b445dabf47f909955c1f51d07761
Stévillis Sousa <stevillis@hotmail.com>
2021-11-05 20:53:37 -0400

    Extend base.html

--- oficina_django/settings.py
+++ oficina_django/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'oficina_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['oficina_django/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
